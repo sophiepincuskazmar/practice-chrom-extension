6e9c25d1bb004ef87fe1200dbf01b28a20976072
Sophie Pincus-Kazmar <spincusk@macalester.edu>
2018-09-24 14:36:27 -0500

    changed color

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#FFFFFF', '#e8453c', '#f9bb2d', '#4688f1'];
+const kButtonColors = ['#3aa757', '#FFFFFF', '#f9bb2d', '#4688f1'];
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
